d6aca68b48222e3d3745e358b63aa001b9d20092
carson Wang-work <carsonwangj@outlook.com>
2017-08-04 01:17:15 +1000

    login button sample

---
+++ SIT313form/SIT313form/SIT313form/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SIT313form
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            string text = MainEntry.Text;
+
+            MainLabel.Text = "Hello" + text;
+        }
+    }
+}
